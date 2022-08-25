0eae70e15079ef3beaab2a5439e16be4eaa9148d
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-24 17:11:59 -0700

    commit tree 593dfc72f3da2c2ef58aa70140c4a0f764c77f7b

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your functions here
+function map(sourceArray, callback) {
+  // init array
+  const x = [];
+
+  // perform operation on every element in sourceArray then push value to init'd array
+  // for (let i = 0; i < sourceArray.length; i++) {
+  //   const indexValue = sourceArray[i];
+  //   x.push(callback(indexValue));
+  // }
+
+  // perform operation on every element in sourceArray then push value to init'd array
+  sourceArray.forEach(i => x.push(callback(i)));
+
+  // return new array
+  return x;
+}
+
+function reduce(sourceArray, callback, initValue) {
+  // init value to return
+  let result;
+
+  // if initValue is defined
+  if (initValue) {
+    // set result to initValue
+    result = initValue;
+
+    // set result to the value of performed operation on every element in sourceArray
+    for (let i = 0; i < sourceArray.length; i++) {
+      result = callback(sourceArray[i], result);
+    }
+    return result
+  } else {
+    // set result to first element in sourceArray
+    result = sourceArray[0];
+    
+    // set result to the value of performed operation on every element in sourceArray
+    for (let i = 1; i < sourceArray.length; i++) {
+      result = callback(sourceArray[i], result);
+    }
+
+    return result
+  }
+}
